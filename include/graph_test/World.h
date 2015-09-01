0024fd6e1daeb7532d389e0edce9b04d1e46fec9
Rokus <rokus@CST110.wtb.tue.nl>
2015-09-01 19:05:48 +0200

    New package for testing graph_map

---
+++ include/graph_test/World.h
@@ -0,0 +1,42 @@
+#ifndef GRAPH_TEST_WORLD_H_
+#define GRAPH_TEST_WORLD_H_
+
+#include <geolib/datatypes.h>
+#include <geolib/Box.h>
+#include <tue/config/configuration.h>
+#include <graph_map/Measurement.h>
+
+// -----------------------------------------------------------------------------------------------
+// Ad Hoc simulator
+// -----------------------------------------------------------------------------------------------
+
+namespace graph_map
+{
+
+struct Object
+{
+    std::string id;
+    geo::Shape shape;
+    geo::Pose3D pose;
+};
+
+// -----------------------------------------------------------------------------------------------
+
+class World
+{
+public:
+    World(){}
+
+    void addObject(Object object);
+
+    void configure(tue::Configuration &config);
+
+    std::vector<graph_map::Measurement> step();
+
+private:
+    std::vector<Object> objects_;
+};
+
+}
+
+#endif
---
+++ src/World.cpp
@@ -0,0 +1,76 @@
+#include "graph_test/World.h"
+
+namespace graph_map
+{
+
+void World::addObject(Object object)
+{
+    objects_.push_back(object);
+}
+
+void World::configure(tue::Configuration &config)
+{
+    if (config.readArray("objects"))
+    {
+        while (config.nextArrayItem())
+        {
+            Object obj;
+
+            // Check for the 'enabled' field. If it exists and the value is 0, omit this object. This allows
+            // the user to easily enable and disable certain objects with one single flag.
+            int enabled;
+            if (config.value("enabled", enabled, tue::OPTIONAL) && !enabled)
+                continue;
+
+            std::string id;
+            if (!config.value("id", id))
+                continue;
+
+
+            // - - - - - - - - - - - - - - - - - - - - - - - -
+            // Load pose
+
+            geo::Pose3D pose = geo::Pose3D::identity();
+            if (config.readGroup("pose", tue::REQUIRED))
+            {
+                config.value("x", pose.t.x);
+                config.value("y", pose.t.y);
+                config.value("z", pose.t.z);
+
+                double roll = 0, pitch = 0, yaw = 0;
+                config.value("roll", roll, tue::OPTIONAL);
+                config.value("pitch", pitch, tue::OPTIONAL);
+                config.value("yaw", yaw, tue::OPTIONAL);
+                pose.R.setRPY(roll, pitch, yaw);
+
+                config.endGroup();
+            }
+            else
+                continue;
+
+
+            // - - - - - - - - - - - - - - - - - - - - - - - -
+            // Add object
+
+            obj.id = id;
+            obj.pose = pose;
+            addObject(obj);
+
+            std::cout << "Added object: id = '" << id << "', pose = " << pose << std::endl;
+
+        }
+
+        config.endArray();
+    }
+}
+
+Measurements World::step()
+{
+    std::cout << "Stepping simulator" << std::endl;
+
+    Measurements measurements;
+
+    return measurements;
+}
+
+}
