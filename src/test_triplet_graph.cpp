28a35b1b42e674a8da77b873c0766bba7c919d4d
Rokus Ottervanger <rokusottervanger@gmail.com>
2015-10-13 12:59:29 +0200

    Split up executables of test_localization and test_triplet_graph

--- src/test_triplet_graph.cpp
+++ src/test_triplet_graph.cpp
@@ -3,4 +3,21 @@
 
 #include <triplet_graph/Graph.h>
 
+int main(int argc, char** argv)
+{
+    // Instantiate graph
+    triplet_graph::Graph graph;
 
+//    // Add nodes to graph
+//    int n1 = graph.addNode("node1");
+//    int n2 = graph.addNode("node2");
+//    int n3 = graph.addNode("node3");
+
+    // Interconnect nodes using Edge2s
+//    graph.addEdge2(n1,n2,3.0);
+//    graph.addEdge2(n2,n3,2.0);
+//    graph.addEdge2(n3,n1,1.0);
+
+    // Interconnect nodes using an Edge3
+//    graph.addEdge3(n1,n2,n3);
+}
